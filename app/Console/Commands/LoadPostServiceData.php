75adc0b992b73e0387940a8540c939d5dfc248a5
Carlos Morfe <cmorfe@alquilando.com>
2022-05-27 13:30:57 -0300

    format data

--- app/Console/Commands/LoadPostServiceData.php
+++ app/Console/Commands/LoadPostServiceData.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use File;
 use Illuminate\Console\Command;
+use Illuminate\Support\LazyCollection;
 
 class LoadPostServiceData extends Command
 {
@@ -30,54 +31,72 @@ class LoadPostServiceData extends Command
    */
   public function handle(): void
   {
-    if (File::missing(storage_path('zip_codes.php')) || true) {
-      $zip_codes = File::lines(base_path('CPdescarga.txt'))
-        ->take(20)
+    $source_path = base_path('CPdescarga.txt');
+
+    $target_path = storage_path('zip_codes.php');
+
+    if (File::missing($target_path)) {
+      $zip_codes = File::lines($source_path)
         ->mapToGroups(
-          function (string $line, $key) {
+          function (string $line) {
             $zip_code = explode('|', $line);
 
-            if ($key > 1 && count($zip_code) > 1) {
-              return [
-                $zip_code[0] => [
-                  'zip_code'       => $zip_code[0],
-                  'locality'       => $zip_code[5],
-                  'federal_entity' => [
-                    'key'  => $zip_code[7],
-                    'name' => $zip_code[4],
-                    'code' => $zip_code[9],
-                  ],
-                  'settlements'    => [
-                    [
-                      'key'             => $zip_code[12],
-                      'name'            => $zip_code[1],
-                      'zone_type'       => $zip_code[13],
-                      'settlement_type' => [
-                        'name' => $zip_code[2],
-                      ],
-                    ],
-                  ],
-                  'municipality'   => [
-                    'key'  => $zip_code[11],
-                    'name' => $zip_code[3],
-                  ],
-                ],
-              ];
+            return $this->buildDataArray($zip_code);
+          }
+        )->mapWithKeys(
+          function (LazyCollection $zip_codes, $key) {
+            $value = $zip_codes->first();
+
+            if ($zip_codes->count() > 1) {
+              $value['settlements'] = $zip_codes->map(
+                function (array $zip_code) {
+                  return $zip_code['settlements'];
+                }
+              );
             }
 
-            return [];
+            return [$key => $value];
           }
-        )->reject(function ($value, $key) {
-          return $key == '';
-        });
+        )->sortKeys();
 
-      File::put(
-        storage_path('zip_codes.php'),
-        "<?php \$zip_codes = unserialize('" . serialize(
-          $zip_codes->toArray()
-        ) . "');" . PHP_EOL
-      );
+      $zip_codes = $zip_codes->toJson();
+
+      $contents
+        = "<?php \$zip_codes = Illuminate\\Support\\LazyCollection::make(json_decode('$zip_codes'));";
+
+      File::put($target_path, $contents);
     }
   }
 
+  /**
+   * @param  array  $zip_code
+   *
+   * @return array[]
+   */
+  function buildDataArray(array $zip_code): array
+  {
+    return [
+      $zip_code[0] => [
+        'zip_code'       => $zip_code[0],
+        'locality'       => strtoupper($zip_code[5]),
+        'federal_entity' => [
+          'key'  => (int)$zip_code[7],
+          'name' => strtoupper($zip_code[4]),
+          'code' => $zip_code[9] == '' ? null : $zip_code[9],
+        ],
+        'settlements'    => [
+          'key'             => (int)$zip_code[12],
+          'name'            => strtoupper($zip_code[1]),
+          'zone_type'       => strtoupper($zip_code[13]),
+          'settlement_type' => [
+            'name' => $zip_code[2],
+          ],
+        ],
+        'municipality'   => [
+          'key'  => (int)$zip_code[11],
+          'name' => strtoupper($zip_code[3]),
+        ],
+      ],
+    ];
+  }
 }
